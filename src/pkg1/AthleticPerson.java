af90fcee9b77c5240f11c1ba201db06b78e4cc75
jyk <jyk@UNC-R90VBEQV.wireless-1x.unc.edu>
2019-08-23 15:27:12 -0400

    Edited to allow delegation.

---
+++ src/pkg1/AthleticPerson.java
@@ -0,0 +1,8 @@
+package pkg1;
+
+public class AthleticPerson implements Person {
+
+	public void unique() {
+		System.out.println("Zoom zoom mf");
+	}
+}
--- src/pkg1/TallPerson.java
+++ src/pkg1/TallPerson.java
@@ -1,20 +1,8 @@
 package pkg1;
 
-public class TallPerson extends Person {
-
-	public TallPerson(int weight, String name, int height, int friendliness) {
-		super(weight, name, height, friendliness);
-	}
+public class TallPerson implements Person {
 	
-	public TallPerson() {
-		weight = 150;
-		height = 75;
-		friendliness = 5;
+	public void unique() {
+		System.out.println("Bow before me you oompa loompa");
 	}
-	
-	public void introduceSelf() {
-		super.introduceSelf();
-		System.out.println("Am I tall? Easy one. Yes.");
-	}
-	
 }
