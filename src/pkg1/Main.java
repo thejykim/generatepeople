4eb847e8f0b98db82529bff2efc0f9048119018e
jyk <jyk@jyk-pc.wireless-1x.unc.edu>
2019-08-31 11:44:58 -0400

    Allows user input for control over Person.

--- src/pkg1/Main.java
+++ src/pkg1/Main.java
@@ -1,22 +1,39 @@
 package pkg1;
 
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		CreatePerson Chris = new CreatePerson(new AthleticPerson());
-		CreatePerson Joey = new CreatePerson(new TallPerson());
-		CreatePerson Boring = new CreatePerson(new DefaultPerson());
+		CreatePerson Person = new CreatePerson(new DefaultPerson());
+		System.out.println("You are now in charge of managing a person. What's the move? Type 'name' to change name.");
+		Scanner input = new Scanner(System.in);
 		
-		// Most of the code below will only be used to test the latest features
-		Chris.giveName("Chris");
-		System.out.println(Chris.getName());
-		Chris.unique();
-		Chris.giveName("Anything else");
-		Joey.giveName("Joey");
-		Joey.unique();
-		System.out.println(Joey.getName());
-		Boring.unique();
+		String setName = "name";
+		String toQuit = "quit";
+		while (true) {
+			String inputString = input.next();
+			if (setName.equals(inputString)) {
+				
+				System.out.println("Okay, changing the name. What name would you like to change your person to?");
+				String changeName = input.next();
+				
+				Person.giveName(changeName);
+				System.out.println("Done! Your person is now known as \"" + changeName + "\".");
+				
+			} else if (toQuit.equals(inputString)) {
+				
+				break;
+				
+			} else {
+				
+				System.out.println("I didn't recognize what you wrote. Can you try again?");
+				
+			}
+		}
+		input.close();
+		System.out.println("Okay, quitting the program. Good bye, hooman...");
 	}
 
 }
